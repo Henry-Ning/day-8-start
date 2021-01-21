7d73b61dba8a378ef4fcf83ea4070795a7512557
Henry Ning <henryhuning@gmail.com>
2021-01-21 08:51:39 +0000

    80. Positional vs. Keyword Arguments 9min

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-# Review: 
-# Create a function called greet(). 
-# Write 3 print statements inside the function.
-# Call the greet() function and run your code.
-
-def greet(name):
+def greet_with(name, location):
   print(f"Hello {name}")
+  print(f"What is it like in {location}?")
 
-#79. Functions with Inputs 10min
-
+greet_with(location="London", name="Angela")
